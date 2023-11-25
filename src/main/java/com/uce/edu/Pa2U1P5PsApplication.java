b207eaca958dc1c79feb2a49d4699e6dc9b29aa4
SimbanaPablo <pablo.simbana01@gmail.com>
2023-11-25 16:35:44 -0500

    cambio en la misma rama

--- src/main/java/com/uce/edu/Pa2U1P5PsApplication.java
+++ src/main/java/com/uce/edu/Pa2U1P5PsApplication.java
@@ -15,7 +15,7 @@ public class Pa2U1P5PsApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		System.out.println("Hola mundo");
 		System.out.println("Soy Pablo Simbaña");
-		
+		System.out.println("cambio en la misma rama");
 	}
 
 }
