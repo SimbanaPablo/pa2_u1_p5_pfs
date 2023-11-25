62215f0ca82fa2947eeee3c11a8bb2c334aa45a4
SimbanaPablo <pablo.simbana01@gmail.com>
2023-11-25 17:11:32 -0500

    taller6

---
+++ src/main/java/com/uce/edu/repository/MateriaRepositoryImp.java
@@ -0,0 +1,31 @@
+package com.uce.edu.repository;
+
+import com.uce.edu.repository.modelo.Materia;
+
+public class MateriaRepositoryImp implements IMateriaRepository {
+
+	@Override
+	public Materia seleccionar(String codigo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insertar(Materia materia) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void actualizar(Materia materia) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void borrar(String codigo) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
